04a57e1ac1da709ff2848645c1a9808197dd6bed
Federico Bricker <fbricker@gmail.com>
2016-05-26 16:16:14 -0300

    Shape iOS part of the extension

--- buildPackage.sh
+++ buildPackage.sh
@@ -4,4 +4,7 @@ cd "$dir"
 PKG_NAME="extension-supersonic"
 
 rm -f "$PKG_NAME.zip"
+rm -rf project/obj
+lime rebuild . ios || exit
+rm -rf project/obj
 zip -r "$PKG_NAME.zip" extension haxelib.json include.xml dependencies
---
+++ frameworks/Supersonic.framework/Versions/A/Headers/SupersonicGender.h
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) 2015 Supersonic. All rights reserved.
+//
+
+#ifndef SUPERSONIC_GENDER_H
+#define SUPERSONIC_GENDER_H
+
+#import <Foundation/Foundation.h>
+
+typedef enum supersonicGender {
+    SUPERSONIC_USER_MALE,
+    SUPERSONIC_USER_FEMALE,
+    SUPERSONIC_USER_UNKNOWN
+} SupersonicGender;
+
+#endif
